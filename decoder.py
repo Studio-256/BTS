e01d3296c178d1a5abc9726e07d6c7090cfc2abc
Timofey X <vuki03@mail.ru>
2020-03-13 12:52:39 +0300

    Test in fight

--- decoder.py
+++ decoder.py
@@ -1,16 +1,47 @@
 #!/usr/bin/python3
-def converter(filein, fileout, filelog):
-    blocksize = 9
+from io import BytesIO
+
+
+def hamming_decode(message: list):
+    count = message.copy()
+    x = 1
+    while x <= len(count):
+        count[x - 1] = 0
+        x *= 2
+    x = 1
+    while x <= len(count):
+        s = 0
+        for i in range(x - 1, len(count), x * 2):
+            for j in range(i, i + x):
+                if j < len(count):
+                    s += count[j]
+        count[x - 1] = s % 2
+        x *= 2
+    x //= 2
+    x0 = x
+    wrong = []
+    while x >= 1:
+        if message[x - 1] != count[x - 1]:
+            wrong.append(x)
+        x //= 2
+    if len(wrong) > 1 and sum(wrong) < len(count):
+        count[sum(wrong) - 1] ^= 1
+    x = x0
+    while x >= 1:
+        c = count.pop(x - 1)
+        x //= 2
+
+    return count
+
+
+def converter(filein: BytesIO, fileout: BytesIO, filelog: BytesIO):
     while True:
-        s = filein.read(blocksize)
-        if not s:
+        a = [filein.read(1) for i in range(3)]
+        if not a[0] or not a[1] or not a[2]:
             break
-        print("convert data", s)
-        s2 = filelog.read(blocksize)
-        if not s2:
-            break
-        fileout.write(s)
-        fileout.write(s2)
-        s = filein.read(blocksize)
-        if not s:
-            break
+        a = [[int(i) for i in bin(int.from_bytes(i, 'big'))[2:].rjust(8, '0')] for i in a]
+        b = [a[0] + a[1][:4], a[1][4:] + a[2]]
+        b = [hamming_decode(i) for i in b]
+        b = [int(''.join(map(str, i)), 2) for i in b]
+        for i in b:
+            fileout.write(i.to_bytes(1, 'big'))

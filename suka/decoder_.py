aa845cea203d3cbcc444828804ce4414a1bdce26
Timofey X <vuki03@mail.ru>
2020-03-13 16:59:55 +0300

    This still works

---
+++ suka/decoder_.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+from io import BytesIO
+
+
+def hamming_decode(message: list):
+    count = message.copy()
+    x = 1
+    while x <= len(count):
+        count[x - 1] = 0
+        x *= 2
+    x = 1
+    while x <= len(count):
+        s = 0
+        for i in range(x - 1, len(count), x * 2):
+            for j in range(i, i + x):
+                if j < len(count):
+                    s += count[j]
+        count[x - 1] = s % 2
+        x *= 2
+    x //= 2
+    x0 = x
+    wrong = []
+    while x >= 1:
+        if message[x - 1] != count[x - 1]:
+            wrong.append(x)
+        x //= 2
+    if len(wrong) > 1 and sum(wrong) < len(count):
+        count[sum(wrong) - 1] ^= 1
+    x = x0
+    while x >= 1:
+        c = count.pop(x - 1)
+        x //= 2
+
+    return count
+
+
+def converter(filein:BytesIO, fileout:BytesIO, filelog:BytesIO):
+    print("convert py")
+
+    while True:
+        s = [filein.read(1) for i in range(3)]
+        if not s[0] or not s[1] or not s[2]:
+            break
+        a = [[int(i) for i in bin(int.from_bytes(i, 'big'))[2:].rjust(8, '0')] for i in s]
+        b = [a[0] + a[1][:4], a[1][4:] + a[2]]
+        b = [hamming_decode(i) for i in b]
+        b = [int(''.join(map(str, i)), 2) for i in b]
+        for i in s:
+            fileout.write(i)
---
+++ suka/encoder_.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from io import BytesIO
+
+
+def hamming_encode(data: list):
+    data = data.copy()
+    x = 1
+    while x <= len(data):
+        data.insert(x - 1, 0)
+        x *= 2
+    x = 1
+    while x <= len(data):
+        s = 0
+        for i in range(x - 1, len(data), x * 2):
+            for j in range(i, i + x):
+                if j < len(data):
+                    s += data[j]
+        data[x - 1] = s % 2
+        x *= 2
+    return data
+
+
+def converter(filein: BytesIO, fileout: BytesIO):
+    while True:
+        s = [filein.read(1) for i in range(3)]
+        if not s[0] or not s[1] or not s[2]:
+            break
+        a = [[int(i) for i in bin(int.from_bytes(i, 'big'))[2:].rjust(8, '0')] for i in s]
+        b = [hamming_encode(i) for i in a]
+        b = [b[0][:8], b[0][8:] + b[1][:4], b[1][4:]]
+        b = [int(''.join(map(str, i)), 2) for i in b]
+        for i in s:
+            fileout.write(i)
